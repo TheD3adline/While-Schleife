cc9f7c7927137ed9195b10b691807cc253b13db9
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-12 18:37:04 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 1;
+
+        //Kopfgesteuerte Schleife
+        while(number <= 5) {
+            System.out.println(number);
+            //number = number + 1;
+            //number += 1;
+            number++;
+        }
+
+    }
+}
